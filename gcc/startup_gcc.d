4a0df145f5c63f9ea39a6938c64bc8488c26aed3
MariaMonserrathEliasSanchez <113149824+monelias@users.noreply.github.com>
2023-01-15 19:13:52 -0600

    Add files via upload

---
+++ gcc/startup_gcc.d
@@ -0,0 +1,9 @@
+gcc/startup_gcc.o: startup_gcc.c \
+ d:\programas\arm\10\ 2021.10\lib\gcc\arm-none-eabi\10.3.1\include\stdint.h \
+ d:\programas\arm\10\ 2021.10\arm-none-eabi\include\stdint.h \
+ d:\programas\arm\10\ 2021.10\arm-none-eabi\include\machine\_default_types.h \
+ d:\programas\arm\10\ 2021.10\arm-none-eabi\include\sys\features.h \
+ d:\programas\arm\10\ 2021.10\arm-none-eabi\include\_newlib_version.h \
+ d:\programas\arm\10\ 2021.10\arm-none-eabi\include\sys\_intsup.h \
+ d:\programas\arm\10\ 2021.10\arm-none-eabi\include\sys\_stdint.h \
+ lib/hw_nvic.h lib/hw_types.h
